1d646a3a2c1800039e37823bec0cf884b9f77402
ljq2278 <811151925@qq.com>
2019-07-31 23:44:54 +0800

    add main

---
+++ main.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+base_model = keras.applications.VGG16(input_shape=(256,256,3), include_top=False)
+
+x = base_model.get_layer("block5_conv3").output
+# inputs = keras.Input(shape=(32,))  # Returns a placeholder tensor
+#
+# # A layer instance is callable on a tensor, and returns a tensor.
+# x = keras.layers.Dense(64, activation='relu')(inputs)
+# x = keras.layers.Dense(64, activation='relu')(x)
+# predictions = keras.layers.Dense(10, activation='softmax')(x)
+#
+# # Instantiate the model given inputs and outputs.
+# model = keras.Model(inputs=inputs, outputs=predictions)
+#
+# # The compile step specifies the training configuration.
+# model.compile(optimizer=tf.train.RMSPropOptimizer(0.001),
+#               loss='categorical_crossentropy',
+#               metrics=['accuracy'])
+#
+# # Trains for 5 epochs
+# model.fit(data, labels, batch_size=32, epochs=5)
